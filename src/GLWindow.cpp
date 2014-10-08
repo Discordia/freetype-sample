3ea89ddaeba1ff687bcddafce9344322962b2526
Robert Sjödahl <robert.sjodahl@ongame.com>
2014-10-08 22:25:10 +0200

    Changed so that desktop also uses OpenGL ES 2.0.

--- src/GLWindow.cpp
+++ src/GLWindow.cpp
@@ -34,10 +34,9 @@ GLWindow::GLWindow(const string& title, const Dimension windowSize, const Dimens
         primaryMonitor = glfwGetPrimaryMonitor();
     }
 
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 0);
 
     window = glfwCreateWindow(windowSize.width, windowSize.height, title.c_str(), primaryMonitor, nullptr);
     if (!window)
