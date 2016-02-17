3cfb5a97764415e2a01eba4ab5854bb75875fae5
Robert Sjödahl <robert.sjodahl@ongame.com>
2016-02-17 12:35:55 +0100

    EGLConfig updates
    Set alpha channel to 8 bit. Depth bugger to 16-bit and stencil buffer to 8-bit.

--- src/EGLWindow.cpp
+++ src/EGLWindow.cpp
@@ -36,6 +36,9 @@ void EGLWindow::init()
         EGL_BLUE_SIZE, 8,
         EGL_GREEN_SIZE, 8,
         EGL_RED_SIZE, 8,
+        EGL_ALPHA_SIZE, 8,
+        EGL_DEPTH_SIZE, 16,
+        EGL_STENCIL_SIZE, 8,
         EGL_NONE
     };
 
